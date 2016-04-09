fcc1b0a52e4bc36e4723b37a6271df69f0958046
Connor Nielsen <connorwnielsen@gmail.com>
2016-04-09 16:53:43 -0700

    stuff

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,56 @@
+'use strict';
+
+
 $(document).ready(function(){
-	
+	var router = new Router();
+	Backbone.history.start({pushState: true});
 });
+
+
+var QuestionCollection = Backbone.Collection.extend({
+	idAttribute: '_id',
+	url: '/api/questions'
+});
+
+var SchoolView = Backbone.View.extend({
+	container: '#app',
+	template: _.template($("#school_template").html()),
+  	initialize: function(){
+  		console.log('School!');
+  		this.fetchQuestions();
+  	},
+  	fetchQuestions: function(){
+  		this.collection = new QuestionCollection();
+  		this.listenTo(this.collection, 'sync', this.render);
+  		this.collection.fetch();
+  	},
+  	render: function(){
+  		this.$el.html(this.template({name: 'Cal Poly SLO'}));
+    	$(this.container).html(this.$el);
+  	}
+});
+
+var QuestionView = Backbone.View.extend({
+	template: _.template($("#question_template").html()),
+  	initialize: function(options){
+  		this.render();
+  	},
+  	render: function(){
+  		this.$el.html(this.template({name: 'Cal Poly SLO'}));
+    	$(this.container).html(this.$el);
+  	}
+});
+
+
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'school',
+  },
+  initialize: function(){
+  	// var nav = new Nav();
+  },
+  school: function(){
+    var school = new SchoolView();
+  }
+});
+
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var mongoConfig   		= require('./mongo-config.js');
 var mongoose   			= require('mongoose');
 var async      			= require('async');
 var config     			= require('./config.js');
+var moment				= require('moment');
 
 
 //reading data in the requests
@@ -15,9 +16,47 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 9000;
 var router = express.Router();
 
+//Question Schema
+var questionSchema = mongoose.Schema({
+  data: String,
+  user_id: String,
+  school_id: String,
+  answers: Array,
+  categories: Array,
+  createdAt: Date
+});
+
+questionSchema.pre('save', function(callback){
+  if (!this.createdAt) { this.createdAt = moment.utc(); }
+  callback();
+});
+
+var Question = mongoose.model('Question', questionSchema);
+
+
 router.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+router.get('/questions', function(req, res) {
+	Question.find({}, function(err, questions){
+		if(err) next(err);
+		res.json(questions);
+	});
+});
+
+router.post('/question', function(req, res) {
+	var question = new Question({
+		data: req.body.data,
+		answers: [],
+		categories: req.body.categories
+	});
+	question.save(function(err, question){
+		if(err) next(err);
+		res.json(question);
+	});
+});
+
 
 app.use(express.static('public'));
 app.use('/api', router);
