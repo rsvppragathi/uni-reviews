74c454999b36eca137cf77af1123b72739089920
Connor Nielsen <connorwnielsen@gmail.com>
2016-04-09 15:05:56 -0700

    config and gitignore and mongodb connection

--- mongoConfig.js
+++ mongoConfig.js
@@ -1,10 +1,12 @@
 var config = require('config');
 var mongoose = require('mongoose');
 
-mongoose.connect(dbUrl, config.db.options);
+mongoose.connect(config.db.url, config.db.options);
+
 mongoose.connection.on('connected', function() {
   console.log('MongoDB Connected!');
 });
+
 mongoose.connection.on('error', function() {
   throw 'MongoDB Connection Error';
 });
---
+++ server.js
@@ -0,0 +1,28 @@
+var express    			= require('express');
+var path    			= require('path');
+var app        			= express();
+var bodyParser 			= require('body-parser');
+var mongoConfig   		= require('mongo-config');
+var mongoose   			= require('mongoose');
+var async      			= require('async');
+var config     			= require('./config.js');
+
+
+//reading data in the requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 9000;
+var router = express.Router();
+
+
+
+router.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.use(express.static('public'));
+app.use('/api', router);
+
+app.listen(port);
+console.log('UniReviews running on port ' + port);
